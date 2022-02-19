6a4b62994d1bf4403fd21bff09221997376980ca
trojanz07 <97985367+trojanz07@users.noreply.github.com>
2022-02-19 11:28:39 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+import Viber from 'helerius';
+
+import config from './config.json';
+
+const client = new Viber();
+
+
+
+
+client.once('ready', () => {
+    console.log('The bot is ready :)');
+})
+
+client.on('messageCreate', message => {
+    if(!message.content.startsWith(prefix)) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    
+    if(command === 'help') {
+        if(args[0] === 'text') {
+            client.sendMessage(config.name, 'Text is Text!', message.author);
+        } else {
+            client.sendMessage(config.name, 'Help is available!', message.author);
+        }
+    }
+})
+
+
+
+client.loadMessageEvent();
+client.addWebhook(8080);
+client.login(config.token, config.webhook);
